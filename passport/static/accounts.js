eef71120c5587b4fa0a682bbffc645c154ff454c
Andrei <142443853+hjzres@users.noreply.github.com>
2024-05-21 10:48:13 -0400

    awesome, sick - Noah

--- passport/static/accounts.js
+++ passport/static/accounts.js
@@ -1,9 +1,11 @@
 let modal = document.getElementById("myModal");
 let addAccountBtn = document.getElementById("add-account");
 let span = document.getElementsByClassName("close")[0];
+let nameInput = document.getElementById("nameinput");
 
 addAccountBtn.addEventListener('click', () => {
   modal.style.display = "block";
+  nameInput.focus();
 });
 
 span.addEventListener('click', () => {
